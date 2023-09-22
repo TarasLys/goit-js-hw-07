8ec8f419c67983a35ca7599594853535815cc896
TarasLys <opros0609@gmail.com>
2023-09-22 20:32:55 +0300

    333

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -53,5 +53,6 @@ container.addEventListener('click', event => {
         document.removeEventListener('keydown', closeModal);
     }
 };
-    // document.addEventListener('keydown', closeModal);
+  document.addEventListener('keydown', closeModal);
+  
 });
